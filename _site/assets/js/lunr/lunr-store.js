4306b655b0534eaeec07492cf70948b7c76990bd
Rose Davies <rosedavies@roses-macbook.home>
2021-05-23 12:12:08 +0100

    New Blog Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,24 @@ var store = [{
         "title": "Showing that solitons carry fractional charge",
         "excerpt":"The concept of a soliton is often only introduced at a graduate level, due to the difficulty of the topics that it appears in. It is probably because of this difficulty that there are many interesting features of a soliton, which is why it’s a shame that it is left...","categories": ["Maths Light","Arguments"],
         "tags": ["Fractional Charge","Solitons"],
-        "url": "http://localhost:4000/maths%20light/arguments/Soliton/",
+        "url": "/maths%20light/arguments/Soliton/",
         "teaser": null
       },{
         "title": "Solving without translational invariance - Green's functions",
         "excerpt":"While I was attempting to rederive some results from a classic 1D transport paper, I got stuck trying to solve for the Green’s function of the system. The main problem wasn’t a horrendous integral or lack of convergence, but that in the system I was considering there was no translational...","categories": ["Maths Heavy","Techniques"],
         "tags": ["Green's Functions","Boundary Conditions"],
-        "url": "http://localhost:4000/maths%20heavy/techniques/Stitching/",
+        "url": "/maths%20heavy/techniques/Stitching/",
         "teaser": null
       },{
         "title": "Solving without translational invariance - Operators",
         "excerpt":"In the previous blog post, a way to stitch together Green’s functions to get an overall non-homogeneous result was explored. The thing that broke translational invariance in that case was a change in the parameters of the system between two different constant values. This is obviously not the only way...","categories": ["Maths Heavy","Techniques"],
         "tags": ["Boundary Conditions","Operators"],
-        "url": "http://localhost:4000/maths%20heavy/techniques/Operators/",
+        "url": "/maths%20heavy/techniques/Operators/",
+        "teaser": null
+      },{
+        "title": "Why the Renormalisation Group is Needed",
+        "excerpt":"The Renormalisation Group (RG) technique is held up as one of the crowning achievements of physics and has obtained a mystical, threatening aura to anyone beginning to learn it. The usual way of explaining it relies on the abstract concept of flows through parameter space and is often explained using...","categories": ["Maths Light","Overview"],
+        "tags": ["Renormalisation Group","Models"],
+        "url": "/maths%20light/overview/RG/",
         "teaser": null
       }]
